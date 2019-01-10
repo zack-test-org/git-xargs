52ee708735521082c1d08c6c493256fd8aa09758
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2019-01-09 21:16:23 -0800

    Allow command to be run with no args

--- release-notes-drafter/main.go
+++ release-notes-drafter/main.go
@@ -21,23 +21,27 @@ import (
 var (
 	// Global flags
 	logLevelFlag = cli.StringFlag{
-		Name:  "loglevel",
-		Value: logrus.InfoLevel.String(),
+		Name:   "loglevel",
+		EnvVar: "RELEASE_NOTES_DRAFTER_LOG_LEVEL",
+		Value:  logrus.InfoLevel.String(),
 	}
 	awsRegionFlag = cli.StringFlag{
-		Name:  "aws-region",
-		Value: "us-east-1",
-		Usage: "AWS region to use for secrets and dynamodb lock table. Defaults to us-east-1.",
+		Name:   "aws-region",
+		EnvVar: "RELEASE_NOTES_DRAFTER_AWS_REGION",
+		Value:  "us-east-1",
+		Usage:  "AWS region to use for secrets and dynamodb lock table. Defaults to us-east-1.",
 	}
 	lockTableFlag = cli.StringFlag{
-		Name:  "lock-table",
-		Value: "release-notes-drafter-locks",
-		Usage: "Name of the dynamodb table holding the synchronization locks.",
+		Name:   "lock-table",
+		EnvVar: "RELEASE_NOTES_DRAFTER_LOCK_TABLE",
+		Value:  "release-notes-drafter-locks",
+		Usage:  "Name of the dynamodb table holding the synchronization locks.",
 	}
 	lockTimeoutFlag = cli.DurationFlag{
-		Name:  "lock-timeout",
-		Value: 10 * time.Minute,
-		Usage: "Amount of time to wait on acquiring the lock before giving up.",
+		Name:   "lock-timeout",
+		EnvVar: "RELEASE_NOTES_DRAFTER_LOCK_TIMEOUT",
+		Value:  10 * time.Minute,
+		Usage:  "Amount of time to wait on acquiring the lock before giving up.",
 	}
 
 	// Subcommand flags
@@ -79,6 +83,10 @@ func main() {
 	app.Author = "Gruntwork <www.gruntwork.io>"
 	app.Description = "A tool to draft release notes based on pull request merge events. Can be used as a web server locally, in lambda, or as a cli action."
 
+	// Default to lambda mode. This is to support the lambda use case, where go runtime for AWS Lambda does not support
+	// CLI args
+	app.Action = serveLambda
+
 	app.Before = initCli
 	app.Flags = []cli.Flag{
 		logLevelFlag,
@@ -101,19 +109,13 @@ func main() {
 			Name:        "local",
 			Usage:       "Local web server",
 			Description: "Start a local webserver that can interpret the github webhook events. This should be hooked to the repository via a github webhook.",
-			Action: func(cliContext *cli.Context) error {
-				serveLocal()
-				return nil
-			},
+			Action:      serveLocal,
 		},
 		{
 			Name:        "lambda",
 			Usage:       "Start an AWS lambda handler",
 			Description: "Start an AWS lambda handler that can interpret the github webhook events. This is intended to be run in the context of an AWS lambda function, with an API gateway. This should be hooked to the repository via a github webhook.",
-			Action: func(cliContext *cli.Context) error {
-				serveLambda()
-				return nil
-			},
+			Action:      serveLambda,
 		},
 	}
 
@@ -121,10 +123,11 @@ func main() {
 }
 
 // serveLambda will serve the handler as a lambda function.
-func serveLambda() {
+func serveLambda(cliContext *cli.Context) error {
 	logger := GetProjectLogger()
 	logger.Infof("Starting as lambda function")
 	lambda.Start(lambdaHandler)
+	return nil
 }
 
 // lambdaHandler does not get a http.Request object, so convert the provided APIGatewayProxyRequest object into the http
@@ -139,11 +142,12 @@ func lambdaHandler(request events.APIGatewayProxyRequest) (Response, error) {
 }
 
 // serveLocal will serve the handler as a basic web server.
-func serveLocal() {
+func serveLocal(cliContext *cli.Context) error {
 	logger := GetProjectLogger()
 	logger.Infof("Starting as local web server")
 	http.HandleFunc("/", httpHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
+	return nil
 }
 
 func httpHandler(writer http.ResponseWriter, request *http.Request) {
