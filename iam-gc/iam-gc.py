6a9e0173130b0694d7b30980246728b2d3656a77
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2019-03-07 04:38:53 -0800

    Polish with docs and dependencies list

--- iam-gc/iam-gc.py
+++ iam-gc/iam-gc.py
@@ -1,4 +1,5 @@
 import boto3
+import argparse
 import re
 import datetime as dt
 import pytz
@@ -7,6 +8,7 @@ from progress.bar import Bar
 
 
 def get_all_roles():
+    """Get all IAM Roles in the account, accounting for pagination in the API"""
     client = boto3.client('iam')
     response = client.list_roles()
     roles = response['Roles']
@@ -17,6 +19,7 @@ def get_all_roles():
 
 
 def delete_role(role):
+    """Delete the given IAM Role"""
     client = boto3.client('iam')
     name = role['RoleName']
     create_date = role['CreateDate']
@@ -25,6 +28,7 @@ def delete_role(role):
 
 
 def get_all_profiles():
+    """Get all IAM Instance Profiles in the account, accounting for pagination in the API"""
     client = boto3.client('iam')
     response = client.list_instance_profiles()
     profiles = response['InstanceProfiles']
@@ -35,6 +39,7 @@ def get_all_profiles():
 
 
 def delete_profile(profile):
+    """Delete the given IAM Instance Profile"""
     client = boto3.client('iam')
     name = profile['InstanceProfileName']
     create_date = profile['CreateDate']
@@ -43,6 +48,7 @@ def delete_profile(profile):
 
 
 def is_test_role_or_instance_profile(name):
+    """Whether or not the given name matches a heuristic list of known test IAM names"""
     regex_list = [
         r'^cloud-nuke-test-.+',
         r'^cloud-nuke-Test.+',
@@ -57,26 +63,50 @@ def is_test_role_or_instance_profile(name):
     return any(re.match(regex, name) for regex in regex_list)
 
 
-def created_before(t):
+def created_before_yesterday(t):
+    """Whether or not the given time is before yesterday (24h ago)"""
     unow = dt.datetime.utcnow().replace(tzinfo=pytz.utc)
     return t < unow - dt.timedelta(hours=24)
 
 
 def want_profile(profile):
+    """Which IAM Instance Profiles we want to delete
+
+    Args:
+        profile (dict) : Dictionary representation of an IAM Instance Profile as returned by boto3.
+
+    Returns:
+        Boolean indicating whether or not we want to delete the given IAM Instance Profile.
+    """
     name = profile['InstanceProfileName']
     create_date = profile['CreateDate']
-    return is_test_role_or_instance_profile(name) and created_before(create_date)
+    return is_test_role_or_instance_profile(name) and created_before_yesterday(create_date)
 
 
 def want_role(role):
+    """Which IAM Roles we want to delete
+
+    Args:
+        profile (dict) : Dictionary representation of an IAM Role as returned by boto3.
+
+    Returns:
+        Boolean indicating whether or not we want to delete the given IAM Role.
+    """
     name = role['RoleName']
     create_date = role['CreateDate']
-    return is_test_role_or_instance_profile(name) and created_before(create_date)
+    return is_test_role_or_instance_profile(name) and created_before_yesterday(create_date)
 
 
 def run_roles(dry=True):
-    roles = get_all_roles()
-    roles = list(filter(want_role, roles))
+    """Run the garbage collection routine for Roles
+
+    This will:
+    - Find all the roles in the account
+    - Filter the test roles based on some heuristics on the name
+    - List out all the roles it found in a table
+    - If in "wet" mode, ask for confirmation from the operator to proceed with the deletion
+    """
+    roles = [role for role in get_all_roles() if want_role(role)]
     print('Found {} roles'.format(len(roles)))
     print('Last role created {}'.format(max(role['CreateDate'] for role in roles).isoformat()))
     roles.sort(key=lambda p: p['CreateDate'], reverse=True)
@@ -98,8 +128,15 @@ def run_roles(dry=True):
 
 
 def run_profiles(dry=True):
-    profiles = get_all_profiles()
-    profiles = list(filter(want_profile, profiles))
+    """Run the garbage collection routine for Instance Profiles.
+
+    This will:
+    - Find all the profiles in the account
+    - Filter the test profiles based on some heuristics on the name
+    - List out all the profiles it found in a table
+    - If in "wet" mode, ask for confirmation from the operator to proceed with the deletion
+    """
+    profiles = [profile for profile in get_all_profiles() if want_profile(profile)]
     print('Found {} profiles'.format(len(profiles)))
     print('Last profile created {}'.format(max(profile['CreateDate'] for profile in profiles).isoformat()))
     profiles.sort(key=lambda p: p['CreateDate'], reverse=True)
@@ -120,10 +157,24 @@ def run_profiles(dry=True):
         delete_profile(profile)
 
 
-def run(dry=True):
+def parse_args():
+    """Parse command line args"""
+    parser = argparse.ArgumentParser(
+        description='This script garbage collects test IAM roles and instance profiles.')
+
+    parser.add_argument('-r', '--run', action='store_true', help='Run the deletion routine.')
+
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+    dry = not args.run
+
     run_profiles(dry)
     run_roles(dry)
 
 
 if __name__ == '__main__':
-    run(dry=True)
+    main()
