041720beb19766d20afab9bd37cd988c683b9753
Yevgeniy Brikman <brikis98@gmail.com>
2019-11-26 22:54:45 +0000

    Add missing cidr_blocks property

--- diagnose/test/fixtures/asg-alb/main.tf
+++ diagnose/test/fixtures/asg-alb/main.tf
@@ -232,6 +232,7 @@ resource "aws_security_group_rule" "allow_all_outbound_instance" {
   from_port         = 0
   to_port           = 0
   protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.instance.id
 }
 
