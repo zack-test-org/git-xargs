50dde0ab903c53f1e451f1c98e78bbd26ac135b0
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2019-12-03 16:45:56 -0800

    Convert sbv files to srt files

---
+++ fixcaptions/sbv2srt.py
@@ -0,0 +1,70 @@
+import click
+import os
+import srt
+import glob
+import datetime
+import re
+from collections import deque
+
+_TIMEDELTA_RE = r'(\d+):(\d+):(\d+).(\d+)'
+
+
+@click.command()
+@click.argument('sbvfile')
+def sbv2srt(sbvfile):
+    if os.path.isdir(sbvfile):
+        sbv_files = glob.glob(os.path.join(sbvfile, '*.sbv'))
+    else:
+        sbv_files = [sbvfile]
+
+    for sbvfile in sbv_files:
+        convert(sbvfile)
+
+
+def convert(sbvfile):
+    target_srt_base, _ = os.path.splitext(sbvfile)
+    target_srt_file = target_srt_base + '.srt'
+
+    with open(sbvfile, 'r') as f:
+        contents = f.read()
+
+    new_srt = []
+
+    lines = deque(contents.splitlines())
+    while lines:
+        cur = lines.popleft()
+        if _is_start_of_caption(cur):
+            raw_start_time, raw_end_time = cur.split(',')
+            start_time = _parse_timedelta(raw_start_time)
+            end_time = _parse_timedelta(raw_end_time)
+            text = lines.popleft()
+            while lines and lines[0]:
+                text += ' ' + lines.popleft()
+            new_srt.append(
+                srt.Subtitle(len(new_srt) + 1, start_time, end_time, text))
+
+    import ipdb
+    ipdb.set_trace()
+
+    with open(target_srt_file, 'w') as f:
+        f.write(srt.compose(new_srt))
+
+
+def _parse_timedelta(timedelta_str):
+    regex = re.compile(_TIMEDELTA_RE)
+    parts = regex.match(timedelta_str)
+    return datetime.timedelta(
+        hours=int(parts.group(1)),
+        minutes=int(parts.group(2)),
+        seconds=int(parts.group(3)),
+        milliseconds=int(parts.group(4)),
+    )
+
+
+def _is_start_of_caption(line):
+    regex = re.compile(_TIMEDELTA_RE + r',' + _TIMEDELTA_RE)
+    return regex.match(line) is not None
+
+
+if __name__ == '__main__':
+    sbv2srt()
