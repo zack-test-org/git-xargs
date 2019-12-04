c4935fed4a9b0c7825ef2b1fbedef0e3664beb94
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2019-12-03 16:53:55 -0800

    Add some code cleanup and documentation

--- fixcaptions/sbv2srt.py
+++ fixcaptions/sbv2srt.py
@@ -25,32 +25,44 @@ def convert(sbvfile):
     target_srt_base, _ = os.path.splitext(sbvfile)
     target_srt_file = target_srt_base + '.srt'
 
+    print('Converting sbv file "{}" to srt file "{}"'.format(
+        sbvfile, target_srt_file))
+
     with open(sbvfile, 'r') as f:
         contents = f.read()
 
     new_srt = []
 
     lines = deque(contents.splitlines())
+    # Keep inspecting each line until we parse all contents of the file
     while lines:
         cur = lines.popleft()
         if _is_start_of_caption(cur):
+            # We are at the start of a caption, so parse it into a Subtitle object.
+
+            # First parse the timestamps
             raw_start_time, raw_end_time = cur.split(',')
             start_time = _parse_timedelta(raw_start_time)
             end_time = _parse_timedelta(raw_end_time)
+
+            # Now parse the text. All text until a blank line are considered a part of this block, so we keep
+            # popleft-ing until we reach an empty line.
             text = lines.popleft()
             while lines and lines[0]:
                 text += ' ' + lines.popleft()
+
+            # We now have everything we need to create the Subtitle object
             new_srt.append(
                 srt.Subtitle(len(new_srt) + 1, start_time, end_time, text))
-
-    import ipdb
-    ipdb.set_trace()
 
     with open(target_srt_file, 'w') as f:
         f.write(srt.compose(new_srt))
 
 
 def _parse_timedelta(timedelta_str):
+    """
+    _parse_timedelta takes a line that looks like HH:MM:SS.milliseconds and converts it to a datetime.timedelta object.
+    """
     regex = re.compile(_TIMEDELTA_RE)
     parts = regex.match(timedelta_str)
     return datetime.timedelta(
@@ -62,6 +74,9 @@ def _parse_timedelta(timedelta_str):
 
 
 def _is_start_of_caption(line):
+    """
+    _is_start_of_caption returns true if this line marks the start of a caption section, which is a timestamp line.
+    """
     regex = re.compile(_TIMEDELTA_RE + r',' + _TIMEDELTA_RE)
     return regex.match(line) is not None
 
