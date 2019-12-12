98bca0ffe57c0e81a4411c1e27a332c2fb8164d6
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2019-12-12 09:06:23 -0800

    Get the client from outside the function

--- recreate-default-vpcs/recreate_default_vpcs.py
+++ recreate-default-vpcs/recreate_default_vpcs.py
@@ -1,30 +1,30 @@
 import boto3
 
+_KNOWN_EXISTING_REGION = 'us-east-1'
+
 
 def get_all_regions():
-    client = boto3.client('ec2', region_name='us-east-1')
+    client = boto3.client('ec2', region_name=_KNOWN_EXISTING_REGION)
     return [region['RegionName'] for region in client.describe_regions()['Regions']]
 
 
-def has_default_vpc(region):
-    client = boto3.client('ec2', region_name=region)
+def has_default_vpc(ec2_client):
     filters = [{'Name': 'isDefault', 'Values': ['true']}]
-    vpcs = client.describe_vpcs(Filters=filters)['Vpcs']
+    vpcs = ec2_client.describe_vpcs(Filters=filters)['Vpcs']
     return len(vpcs) > 0
 
 
-def create_default_vpc(region):
-    client = boto3.client('ec2', region_name=region)
-    return client.create_default_vpc()['Vpc']
+def create_default_vpc(ec2_client):
+    return ec2_client.create_default_vpc()['Vpc']
 
 
-def create_default_vpc_if_not_exist(region):
-    if has_default_vpc(region):
+def create_default_vpc_if_not_exist(ec2_client, region):
+    if has_default_vpc(ec2_client):
         print('Region {} has default VPC. Skipping create.'.format(region))
         return
 
     print('Region {} does not have default VPC. Creating.'.format(region))
-    vpc = create_default_vpc(region)
+    vpc = create_default_vpc(ec2_client)
     print('Created VPC {}'.format(vpc['VpcId']))
 
 
@@ -35,7 +35,8 @@ def main():
     print('Found {} regions'.format(len(regions)))
     print('Ensuring default VPC exists in each region')
     for region in regions:
-        create_default_vpc_if_not_exist(region)
+        client = boto3.client('ec2', region_name=region)
+        create_default_vpc_if_not_exist(client)
 
 
 if __name__ == '__main__':
