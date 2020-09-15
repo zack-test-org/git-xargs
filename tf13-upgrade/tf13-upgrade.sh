fae1105b48780b61c00ec92d1dfabe749a94d070
Yevgeniy Brikman <brikis98@gmail.com>
2020-09-15 15:09:23 +0100

    Update script to patch versions.tf

--- tf13-upgrade/tf13-upgrade.sh
+++ tf13-upgrade/tf13-upgrade.sh
@@ -23,7 +23,35 @@ terraform_folders_str=$(find "$working_dir" -not -path '*/\.*' -type f -name '*.
 # Split a newline-separated string into an array: https://stackoverflow.com/a/24426608/483528
 IFS=$'\n' read -d '' -ra terraform_folders_arr < <(printf '%s\0' "$terraform_folders_str")
 
+required_version_regex='required_version = ">= 0.13"'
+required_version_replacement_first_line="This module is now only being tested with Terraform 0.13.x. However, to make upgrading easier, we are setting"
+# Note that the backslashes and newlines here in the middle of the string below are intentional! That's because we use
+# this variable with sed, and on MacOS, sed does not support '\n' in replacement text, but a backslash followed by a
+# literal newline should work: https://superuser.com/a/307486
+required_version_replacement="\\
+  # $required_version_replacement_first_line\\
+  # 0.12.20 as the minimum version, as that version added support for required_providers, making it forwards compatible\\
+  # with 0.13.x code.\\
+  required_version = \">= 0.12.20\""
+
 for folder in "${terraform_folders_arr[@]}"; do
-  echo "Running terraform 0.13upgrade in '$folder'"
-  terraform 0.13upgrade -yes "$folder"
+  versions_file="$folder/versions.tf"
+
+  # Try to make this script idempotent by not re-upgrading code that has already been upgraded and has our comment in
+  # it indicating that we've patched the versions.tf file.
+  if grep -q "$required_version_replacement_first_line" "$versions_file" > /dev/null 2>&1; then
+    echo "We've already upgraded the code in '$folder'. Will not upgrade again."
+  else
+    echo "Running terraform 0.13upgrade in '$folder'"
+    terraform 0.13upgrade -yes "$folder"
+
+    # The 0.13upgrade command should have created this file
+    if [[ ! -f "$versions_file" ]]; then
+      echo "ERROR: Expected file '$versions_file' does not exist!"
+      exit 1
+    fi
+
+    echo "Overwriting version constraint in '$versions_file' to indicate the code is still compatible with TF 0.12"
+    sed -i '' "s/$required_version_regex/$required_version_replacement/g" "$versions_file"
+  fi
 done
